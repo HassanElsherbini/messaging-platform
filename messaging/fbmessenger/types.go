f2f67bc304a581aea17a359869bafaba4817fb7d
Hassan Elsherbini <h.elsherbini7@gmail.com>
2022-07-28 17:55:49 -0400

    Model fb messenger received message event types (msg read, feedback and text message)

--- messaging/fbmessenger/types.go
+++ messaging/fbmessenger/types.go
@@ -1,1 +1,61 @@
 package fbmessenger
+
+type Event struct {
+	Object  string       `json:"object"`
+	Entries []EventEntry `json:"entry"`
+}
+
+type EventEntry struct {
+	ID        string         `json:"id"`
+	Time      int            `json:"time"`
+	Messaging []MessageEvent `json:"messaging"`
+}
+
+type MessageEvent struct {
+	Sender    MessageSender    `json:"sender"`
+	Recipient MessageRecipient `json:"recipient"`
+	TimeStamp int              `json:"timestamp"`
+	Message   *ReceivedMessage `json:"message"`
+
+	Feedback *Feedback `json:"messaging_feedback"`
+
+	Read *Read `json:"read"`
+}
+
+type MessageRecipient struct {
+	ID string `json:"id"`
+}
+
+type MessageSender struct {
+	ID string `json:"id"`
+}
+
+type ReceivedMessage struct {
+	Mid  string `json:"mid,omitempty"`
+	Seq  int    `json:"seq,omitempty"`
+	Text string `json:"text"`
+}
+
+type Feedback struct {
+	FeedbackScreens []FeedbackScreen `json:"feedback_screens"`
+}
+
+type FeedbackScreen struct {
+	ScreenID  int                         `json:"screen_id"`
+	Questions map[string]FeedbackQuestion `json:"questions"`
+}
+
+type FeedbackQuestion struct {
+	Payload  string                    `json:"payload"`
+	Type     string                    `json:"type"`
+	FollowUp *FeedbackQuestionFollowUp `json:"follow_up"`
+}
+
+type FeedbackQuestionFollowUp struct {
+	Payload string `json:"payload"`
+	Type    string `json:"type"`
+}
+
+type Read struct {
+	Watermark int64 `json:"watermark"`
+}
